c9acf2220af1aa7e2608b4b887b7bc7cefeaa4fc
mohammad <mohammadjv94@gmail.com>
2018-03-03 00:13:46 +0330

    git ignore modified

---
+++ src/char_recognition/scripts/charRecognition.py
@@ -0,0 +1,9 @@
+import numpy
+import cv2
+
+class CharRecognition:
+	def __init__(self):
+		print "char_recognition module created!"
+	def find_character_type(self, image):
+		print 'toooodooooo'
+		return 'h'
