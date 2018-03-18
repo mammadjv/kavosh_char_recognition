1e7abbc9bf422d981d2252d90f231a6f5b38b516
mammadjv <mohammadjv94@gmail.com>
2018-03-18 18:22:46 +0000

    write data to serial port is ok

--- src/serial_connection/scripts/serialConnectorBase.py
+++ src/serial_connection/scripts/serialConnectorBase.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 
 import rospy
-from serialConnector import SerialConnector
+#from serialConnector import SerialConnector
 from std_msgs.msg import String
 from std_msgs.msg import Bool
+import time
+import serial
 
-class SerialConnectorBase(SerialConnector):
+class SerialConnectorBase:
 	def __init__(self):
 		rospy.init_node('serial_connector_node', anonymous=True)
-		SerialConnector.__init__(self)
+#		SerialConnector.__init__(self)
 		self.state_subscriber = rospy.Subscriber('/char_type',String, self.on_char_type_received)
 		self.life_cycle_publisher = rospy.Publisher('/life_cycle_state',Bool,queue_size = 10)
+		self.serialConnector = serial.Serial(port=' /dev/ttyS0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0 \
+		        baudrate = 9600, \
+		        parity=serial.PARITY_NONE, \
+		        stopbits=serial.STOPBITS_ONE, \
+		        bytesize=serial.EIGHTBITS, \
+		        timeout=1)
+
 	def on_char_type_received(self, char_type):
-#		print "receive the char type"
-		self.write_data(char_type.data)
+		self.serialConnector.write(char_type)
 		life_cycle_state = Bool()
 		life_cycle_state.data = True
 		self.life_cycle_publisher.publish(life_cycle_state)
