f69dac8df9c6d0f74ece51d60f2d5c46b62f0595
mammadjv <mohammadjv94@gmail.com>
2018-03-18 19:43:44 +0000

    pattern perceptor module added for char recognition

--- src/char_recognition/scripts/charRecognition.py
+++ src/char_recognition/scripts/charRecognition.py
@@ -1,11 +1,16 @@
 import numpy
 import cv2
-#import caffe
+import caffe
+from pattern_perceptor import PatternPerceptor
+
 
 
 class CharRecognition:
 	def __init__(self):
 		print "char_recognition module created!"
+		self.pattern_perceptor = PatternPerceptor('/home/ubuntu/Kavosh_Char_Recognition/src/char_recognition/scripts/deploy.prototxt','/home/ubuntu/Kavosh_Char_Recognition/src/char_recognition/scripts/hand_written1.caffemodel')
 	def find_character_type(self, image):
-		print 'toooodooooo'
-		return 'h'
+		return str(self.pattern_perceptor.recognize(image))
+
+
+#char = CharRecognition()
--- src/char_recognition/scripts/charRecognitionBase.py
+++ src/char_recognition/scripts/charRecognitionBase.py
@@ -2,7 +2,7 @@
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Bool
-from system_messages.msg import Image
+from system_messages.msg import ImageMsg
 from charRecognition import CharRecognition
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
@@ -11,11 +11,13 @@ import cv2
 class CharRecognitionBase(CharRecognition):
 	def __init__(self):
 		CharRecognition.__init__(self)
-		self.image_subscriber = rospy.Subscriber("/contour", Image, self.on_image_received)
+		self.image_subscriber = rospy.Subscriber("/contour", ImageMsg, self.on_image_received)
 		self.char_type_publisher = rospy.Publisher("/char_type",String,queue_size=10)
 
 	def on_image_received(self, image):
 		rgb_image = CvBridge().imgmsg_to_cv2(image.rgb, "bgr8")
+#		cv2.imshow('rgb',rgb_image);
+#		cv2.waitKey(1)
 		char_type = self.find_character_type(rgb_image)
 		char_type_msg = String()
 		char_type_msg.data = char_type
