23495d682b2cc2c493a5c44217a49f7bf0ae5a02
mammadjv <mohammadjv94@gmail.com>
2018-03-28 13:45:02 +0000

    camera module integration with raspi_cam ros module

--- src/camera/scripts/cameraBase.py
+++ src/camera/scripts/cameraBase.py
@@ -1,48 +1,50 @@
 #!/usr/bin/env python
 
-import picamera
 import rospy
 from camera import Camera
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import *
 from std_msgs.msg import Bool
 from  system_messages.msg import ImageMsg
+import time
+import numpy as np
 import time
 
 
 class CameraBase(Camera):
 	def __init__(self):
-#		rospy.init_node('camera_node', anonymous=True)
 		Camera.__init__(self)
 		self.image_publisher = rospy.Publisher('/image', ImageMsg, queue_size = 10)
+		self.image_subscriber = rospy.Subscriber('raspicam_node/image/compressed', CompressedImage,self.on_image_received)
 		self.bool_publiher = rospy.Publisher('/bool', Bool, queue_size = 10)
 		self.state_subscriber = rospy.Subscriber('/life_cycle_state',Bool, self.on_life_cycle_state_changed)
 		self.bridge = CvBridge()
-		self.start_camera_buffering()
-		self.image = cv2.imread('/home/ubuntu/Kavosh_Char_Recognition/picture.png')
-#		self.on_life_cycle_state_changed(True)
+		self.compressed_images = list()
+
+
+	def on_image_received(self, compressed_image):
+		if(len(self.compressed_images) == 10):
+			self.compressed_images.pop(0)
+		self.compressed_images.append(compressed_image)
 
 	def on_life_cycle_state_changed(self, life_cycle_state):
-#		while not rospy.is_shutdown():
-		bool_msg = Bool()
-		bool_msg.data = True
-		self.bool_publiher.publish(bool_msg)
 		image_message = ImageMsg()
-		image_message.rgb = self.bridge.cv2_to_imgmsg(self.image, "bgr8")
-		image_message.image_is_prepared = True
+		image = None
+		if(len(self.compressed_images) > 0):
+			first = time.time()
+			compressed_image = self.compressed_images.pop()
+			np_arr = np.fromstring(compressed_image.data, np.uint8)
+			image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+			print time.time() - first
+		if(image != None):
+			image_message.rgb = self.bridge.cv2_to_imgmsg(image, "bgr8")
+			image_message.image_is_prepared = True
+		else:
+			image_message.image_is_prepared = False
 		self.image_publisher.publish(image_message)
-#			print "publish the next image from buffer"
-
+		
 if __name__ == '__main__':
 	rospy.init_node('camera_node', anonymous=True)
-	with picamera.PiCamera() as camera:
-		for an in camera.capture_continuous(stream):
-			stream.truncate()
-			stream.seek(0)
-			if process(stream):
-				break
-			time.sleep(0.5)
-#	camera = CameraBase()
-#	camera.on_life_cycle_state_changed(True)
+	camera = CameraBase()
 	rospy.spin()
