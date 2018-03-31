2422b595c4afac7d430cc44ad67c491ce89753d9
mammadjv <mohammadjv94@gmail.com>
2018-03-31 12:28:26 +0430

    Update charRecognition.py

--- src/char_recognition/scripts/charRecognition.py
+++ src/char_recognition/scripts/charRecognition.py
@@ -8,7 +8,7 @@ from pattern_perceptor import PatternPerceptor
 class CharRecognition:
 	def __init__(self):
 		print "char_recognition module created!"
-		self.pattern_perceptor = PatternPerceptor('/home/ubuntu/kavosh_Char_Recognition/src/char_recognition/scripts/deploy.prototxt','/home/ubuntu/kavosh_Char_Recognition/src/char_recognition/scripts/hand_written_99.caffemodel')
+		self.pattern_perceptor = PatternPerceptor('/home/ubuntu/kavosh_char_recognition/src/char_recognition/scripts/deploy.prototxt','/home/ubuntu/kavosh_char_recognition/src/char_recognition/scripts/hand_written_99.caffemodel')
 	def find_character_type(self, image):
 #		return 's'
 		output_number = self.pattern_perceptor.recognize(image)
