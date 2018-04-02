79459670f1b2f3fbe304ca3b0cd73ff58c232f89
mammadjv <mohammadjv94@gmail.com>
2018-04-02 15:34:37 +0000

    gpio connector implemented

--- src/serial_connection/scripts/serialConnectorBase.py
+++ src/serial_connection/scripts/serialConnectorBase.py
@@ -5,31 +5,56 @@ import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Bool
 import time
-import serial
+#import serial
+import RPi.GPIO as GPIO
 
-class SerialConnectorBase:
-	def __init__(self):
+#ser = serial.Serial(port = '/dev/ttyS0',baudrate = 9600, parity = serial.PARITY_NONE, bytesize = serial.EIGHTBITS, timeout = 1)
+
+class GPIOConnector:
+	def __init__(self , id1 , id2 , id3):
+		self.pinid1 = id1
+		self.pinid2 = id2 
+		self.pinidRead = id3
 		rospy.init_node('serial_connector_node', anonymous=True)
-#		SerialConnector.__init__(self)
 		self.state_subscriber = rospy.Subscriber('/char_type',String, self.on_char_type_received)
+		self.reset_gpio_subscriber = rospy.Subscriber('/write_gpio',Bool, self.reset)
 		self.life_cycle_publisher = rospy.Publisher('/life_cycle_state',Bool,queue_size = 10)
-		return
-		self.serialConnector = serial.Serial(port=' /dev/ttyS0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0 \
-		        baudrate = 9600, \
-		        parity=serial.PARITY_NONE, \
-		        stopbits=serial.STOPBITS_ONE, \
-		        bytesize=serial.EIGHTBITS, \
-		        timeout=1)
-
-	def on_char_type_received(self, char_type):
-#		self.serialConnector.write(char_type)
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.pinid1 , GPIO.OUT)
+		GPIO.setup(self.pinid2 , GPIO.OUT)
+		GPIO.setup(self.pinidRead , GPIO.IN, pull_up_down=GPIO.PUD_DOWN )
+		self.reset("annn")
+	def writeData(self, pin1value , pin2value):
+		GPIO.output(self.pinid1,pin1value)
+#		GPIO.output(self.pinid1,GPIO.HIGH)
+		time.sleep(0.002)
+		GPIO.output(self.pinid2,pin2value)
+#		GPIO.output(self.pinid2,GPIO.HIGH)
+		time.sleep(0.002)
+	def readData(self):
+		return GPIO.input(self.pinidRead)
+	def reset(self, data):
+		print "reset             !!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+		GPIO.output(self.pinid1,0)
+		time.sleep(0.002)
+		GPIO.output(self.pinid2,0)
+		time.sleep(0.002)
+	def on_char_type_received(self,char_type):
+		print "wriiiiiiiiiiiteee  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+		if(char_type.data == "H"):
+		        self.writeData(0,1)
+		if(char_type.data == "S"):
+		        self.writeData(1,0)
+		if(char_type.data == "U"):
+		        self.writeData(1,1)
+#		self.writeData(0, 1)
+#		print("GPIO!!!")
 		life_cycle_state = Bool()
 		life_cycle_state.data = True
 		self.life_cycle_publisher.publish(life_cycle_state)
 		
 
 if __name__ == '__main__':
-	serialConnector = SerialConnectorBase()
-	serialConnector.on_char_type_received('a')
-	print "data is written"
+	gpioConnector = GPIOConnector(23,24,18)
+	gpioConnector.writeData(1,0)
 	rospy.spin()
